f31beae85c096bfb6336c9fdee02287649c2f9e4
minhajul <minhajul@maya.com.bd>
2017-05-05 22:05:09 +0600

    Getting form data

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,58 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 class Contact extends Component{
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {value: ''};
+    //
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    // }
+    //
+    // handleChange(event) {
+    //     this.setState({value: event.target.value});
+    // }
+    //
+    // handleSubmit(event) {
+    //     console.log('A name was submitted: ' + this.state.value);
+    //     event.preventDefault();
+    // }
+
+    constructor(props){
+        super(props);
+        this.state = { name: '', email: '', password: '' };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleNameChange(event){
+        this.setState({ name : event.target.value })
+    }
+
+    handleEmailChange(event){
+        this.setState({ email : event.target.value })
+    }
+
+    handlePasswordChange(event){
+        this.setState({ password : event.target.value })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        const formData = {
+           name : this.state.name,
+           email : this.state.email,
+           password : this.state.password,
+        };
+        
+        console.log(formData);
+    }
+
+
     render(){
         return(
             <div className="container">
@@ -10,23 +62,23 @@ class Contact extends Component{
                 <div className="col-md-8 col-md-offset-2">
                     <h2>Sign Up Form</h2>
                     <hr/>
-                    <form className="form-horizontal">
+                    <form className="form-horizontal" onSubmit={ this.handleSubmit }>
                         <div className="form-group">
                             <label htmlFor="name" className="col-sm-2 control-label">Name</label>
                             <div className="col-sm-10">
-                                <input type="text" className="form-control" placeholder="Email"/>
+                                <input type="text" className="form-control" value={this.state.name} placeholder="Name" onChange={ this.handleNameChange } />
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email" className="col-sm-2 control-label">Email</label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control" placeholder="Email"/>
+                                <input type="email" className="form-control" value={this.state.email} placeholder="Email" onChange={ this.handleEmailChange }/>
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="password" className="col-sm-2 control-label">Password</label>
                             <div className="col-sm-10">
-                                <input type="password" className="form-control" placeholder="Password"/>
+                                <input type="password" className="form-control" value={this.state.password} placeholder="Password" onChange={ this.handlePasswordChange }/>
                             </div>
                         </div>
                         <div className="form-group">
