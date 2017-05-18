a66c1d2d95b28c79877b25c923935c58a816b7c5
minhajul <minhajul@maya.com.bd>
2017-05-18 21:39:18 +0600

    Updated about us section

--- src/components/partials/AboutMe.js
+++ src/components/partials/AboutMe.js
@@ -8,39 +8,37 @@ class AboutMe extends Component{
             month = (new Date(created_at)).getMonth()+1,
             day = (new Date(created_at)).getDate();
         return(
-            <div>
-                <div className="well well-sm">
-                    <div className="row">
-                        <div className="col-sm-8 col-md-6">
-                            <img src={this.props.aboutMe.avatar_url} alt="" className="img-rounded img-responsive" />
-                        </div>
-                        <div className="col-sm-4 col-md-6">
-                            <h4>
-                                {this.props.aboutMe.name}
-                            </h4>
-                            <small>
-                                <cite title="San Francisco, USA">
-                                    {this.props.aboutMe.location }
-                                </cite>
-                            </small>
-                            <br/>
-                            <p>
-                                Email: kmsohelinfo@gmail.com
-                                <br />
-                                Blog:
-                                <a href={this.props.aboutMe.blog}>
-                                    {this.props.aboutMe.blog}
-                                </a>
-                                <br />
-                                Created At: { year + '-' + month + '-' + day }
-                            </p>
-                            <div className="btn-group">
-                                <ul className="list-inline">
-                                    <li><a href="https://twitter.com/minhaj_cse">Twitter</a></li>
-                                    <li><a href="https://www.facebook.com/minhajul.islamsohel">Facebook</a></li>
-                                    <li><a href="https://github.com/minhajCSE">Github</a></li>
-                                </ul>
-                            </div>
+            <div className="well well-sm">
+                <div className="row">
+                    <div className="col-sm-8 col-md-6">
+                        <img src={this.props.aboutMe.avatar_url} alt="" className="img-rounded img-responsive" />
+                    </div>
+                    <div className="col-sm-4 col-md-6">
+                        <h4>
+                            { this.props.aboutMe.name }
+                        </h4>
+                        <small>
+                            <cite title={ this.props.aboutMe.location }>
+                                { this.props.aboutMe.location }
+                            </cite>
+                        </small>
+                        <br/>
+                        <p>
+                            Email: kmsohelinfo@gmail.com
+                            <br />
+                            Blog:
+                            <a href={this.props.aboutMe.blog}>
+                                {this.props.aboutMe.blog}
+                            </a>
+                            <br />
+                            Created At: { year + '-' + month + '-' + day }
+                        </p>
+                        <div className="btn-group">
+                            <ul className="list-inline">
+                                <li><a target="_blank" href="https://twitter.com/minhaj_cse">Twitter</a></li>
+                                <li><a target="_blank" href="https://www.facebook.com/minhajul.islamsohel">Facebook</a></li>
+                                <li><a target="_blank" href="https://github.com/minhajCSE">Github</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
