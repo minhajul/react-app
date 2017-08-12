37a47fed6e9b164ddf9e57525a9a500f6a0b5df7
minhajul <minhajul@maya.com.bd>
2017-08-12 19:59:52 +0600

    Updated column

--- src/components/partials/SinglePost.js
+++ src/components/partials/SinglePost.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SinglePost extends Component {
     render() {
         return (
-            <div className="col-lg-12">
+            <div className="col-lg-6">
                 <h4>{this.props.post.title}</h4>
                 <p>{this.props.post.description}</p>
             </div>
